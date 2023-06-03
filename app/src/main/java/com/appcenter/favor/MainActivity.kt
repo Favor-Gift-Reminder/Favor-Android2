3c04f4ce045490d8624ada5b1454016a4bf1f985
YangNu <bin450579@gmail.com>
2023-06-03 13:24:23 +0900

    Init. Activity Main

---
+++ app/src/main/java/com/appcenter/favor/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.appcenter.favor
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.appcenter.favor.UI.OnBoard.OnboardFragment
+import com.appcenter.favor.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(mainLooper).postDelayed({
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                .replace(binding.container.id, OnboardFragment())
+                .commit()
+        }, 1500)
+    }
+}
