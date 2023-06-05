19423d6ca36616f4e8e77387c3d278a0b80f3b92
YangNu <bin450579@gmail.com>
2023-06-05 17:07:10 +0900

    Init. FragmentReminderDialog 생성

---
+++ app/src/main/java/com/appcenter/favor/UI/Reminder/ReminderDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.appcenter.favor.UI.Reminder
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.appcenter.favor.R
+
+class ReminderDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        return inflater.inflate(R.layout.fragment_reminder_dialog, container, false)
+    }
+}
