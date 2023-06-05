d439c4727d8339b597871e73c115c78cbf059ad3
YangNu <bin450579@gmail.com>
2023-06-05 15:19:59 +0900

    Feat. SearchFragment 기본 기능 구현

--- app/src/main/java/com/appcenter/favor/UI/Search/SearchFragment.kt
+++ app/src/main/java/com/appcenter/favor/UI/Search/SearchFragment.kt
@@ -1,60 +1,92 @@
 package com.appcenter.favor.UI.Search
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.appcenter.favor.R
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.OnBackPressedCallback
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.appcenter.favor.MainActivity
+import com.appcenter.favor.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentSearchBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var parentContext: MainActivity
+    private lateinit var onBackPressed: OnBackPressedCallback
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentContext = context as MainActivity
+
+        onBackPressed = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                parentContext.supportFragmentManager.beginTransaction()
+                    .remove(this@SearchFragment)
+                    .commit()
+            }
         }
+        requireActivity().onBackPressedDispatcher.addCallback(this, onBackPressed)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        onBackPressed.remove()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        init_toolbar()
+        binding.root.setOnClickListener {}
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun init_toolbar() {
+        binding.toolbar.back.setOnClickListener { onBackPressed.handleOnBackPressed() }
+
+        binding.toolbar.editSub.setOnClickListener { binding.toolbar.edit.setText("") }
+
+        var inUse = false
+        binding.toolbar.edit.addTextChangedListener {
+            if (it.toString().isNotBlank()) {
+                if(inUse) return@addTextChangedListener
+                binding.toolbar.editSub.visibility = View.VISIBLE
+                childFragmentManager.beginTransaction()
+                    .replace(binding.container.id, SearchRecentFragment())
+                    .commit()
             }
+            else {
+                binding.toolbar.editSub.visibility = View.GONE
+                childFragmentManager.beginTransaction()
+                    .replace(binding.container.id, SearchMainFragment())
+                    .commit()
+            }
+            inUse = it.toString().isNotBlank()
+        }
+
+        binding.toolbar.edit.setOnEditorActionListener { _, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                if (binding.toolbar.edit.text.toString().isBlank()) return@setOnEditorActionListener true
+                childFragmentManager.beginTransaction()
+                    .replace(binding.container.id, SearchTabFragment())
+                    .commit()
+                hideKey()
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+    }
+
+    private fun hideKey() {
+        val imm = parentContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(parentContext.currentFocus?.windowToken, 0)
     }
 }
