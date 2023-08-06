f7be8f7c43c577f7e5ec792b8492b396ca3fffee
DESKTOP-FDBRA0R\mscho <mscho>
2023-08-06 11:14:14 +0900

    Add. 선뭁친구등록

--- app/src/main/java/com/appcenter/favor/UI/Gift/GiftFriendFragment.kt
+++ app/src/main/java/com/appcenter/favor/UI/Gift/GiftFriendFragment.kt
@@ -1,19 +1,51 @@
 package com.appcenter.favor.UI.Gift
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import com.appcenter.favor.MainActivity
 import com.appcenter.favor.R
+import com.appcenter.favor.databinding.FragmentGiftBinding
+import com.appcenter.favor.databinding.FragmentGiftFriendBinding
 
 class GiftFriendFragment : Fragment() {
+    private lateinit var binding : FragmentGiftFriendBinding
 
+    private lateinit var parentContext: MainActivity
+    private lateinit var onBackPressed: OnBackPressedCallback
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentContext = context as MainActivity
+        onBackPressed = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                parentFragmentManager.beginTransaction()
+                    .remove(this@GiftFriendFragment)
+                    .commit()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, onBackPressed)
+    }
+    override fun onDetach() {
+        super.onDetach()
+        onBackPressed.remove()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gift_friend, container, false)
+        binding = FragmentGiftFriendBinding.inflate(inflater, container, false)
+        init_toolbar()
+        return binding.root
+    }
+
+    private fun init_toolbar() {
+        binding.toolbar.back.setOnClickListener { onBackPressed.handleOnBackPressed() }
+        binding.toolbar.back.setImageResource(R.drawable.icon_arrow_left)
+        binding.toolbar.icon.setImageResource(R.drawable.icon_default_friend)
+        binding.toolbar.sub1.hint = "등록"
     }
 }
