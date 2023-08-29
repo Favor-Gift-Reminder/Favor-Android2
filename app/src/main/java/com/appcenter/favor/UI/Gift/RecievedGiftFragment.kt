d6bfec423b42727b2e214941ad68f7da769ccd3e
DESKTOP-FDBRA0R\mscho <mscho>
2023-08-29 21:28:41 +0900

    Feat 받은선물 등록 UI수정

--- app/src/main/java/com/appcenter/favor/UI/Gift/RecievedGiftFragment.kt
+++ app/src/main/java/com/appcenter/favor/UI/Gift/RecievedGiftFragment.kt
@@ -5,11 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
+import com.appcenter.favor.MainActivity
 import com.appcenter.favor.R
+import com.appcenter.favor.databinding.FragmentGivenGiftBinding
+import com.appcenter.favor.databinding.FragmentRecievedGiftBinding
+import com.nise.favor_android.Interface.GiftDTO.GiftRequestDTO
 
 
 class RecievedGiftFragment : Fragment() {
+    private lateinit var binding: FragmentRecievedGiftBinding
 
+    private lateinit var parentContext: MainActivity
+    private lateinit var onBackPressed: OnBackPressedCallback
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,6 +29,53 @@ class RecievedGiftFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recieved_gift, container, false)
+        val giftName = binding.giftName.text
+
+        binding.apply {
+            btnSelectFriend.setOnClickListener {
+                val giftFriendFragment = GiftFriendFragment()
+                val supportFragmentManager = requireActivity().supportFragmentManager
+                supportFragmentManager.setFragmentResultListener(
+                    "REQUEST",
+                    viewLifecycleOwner
+                ) { resultKey, bundle ->
+                    if (resultKey == "REQUEST") {
+                        val friend = bundle.getString("SELECT_FRIEND")
+                        binding.btnSelectFriend.text = friend
+                        binding.btnSelectFriend.setTextColor(
+                            ContextCompat.getColor(
+                                requireActivity(),
+                                R.color.icon
+                            )
+                        )
+                    }
+                }
+                parentFragmentManager.beginTransaction()
+                    .add(parentContext.binding.container.id, GiftFriendFragment())
+                    .commit()
+            }
+
+            btnSelectDate.setOnClickListener {
+                val datePickerFragment = DatePickerFragment()
+                val supportFragmentManager = requireActivity().supportFragmentManager
+                supportFragmentManager.setFragmentResultListener(
+                    "REQUEST_KEY",
+                    viewLifecycleOwner
+                ) { resultKey, bundle ->
+                    if (resultKey == "REQUEST_KEY") {
+                        val date = bundle.getString("SELECTED_DATE")
+                        binding.btnSelectDate.text = date
+                        binding.btnSelectDate.setTextColor(ContextCompat.getColor(requireActivity(), R.color.icon))
+                    }
+                }
+                // show
+                datePickerFragment.show(supportFragmentManager, "DatePickerFragment")
+            }
+            val list = MutableList<Int>(3, {i -> i})
+            val giftRequest = GiftRequestDTO(
+                "a","b", list,binding.btnSelectDate.toString(),"a", giftName.toString(),false,true
+            )
+        }
     }
 
 }
